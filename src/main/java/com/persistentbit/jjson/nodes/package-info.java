8359210bfb1710fa00439cbabb22351d089695ba
Peter Muys <pmu@schaubroeck.be>
2016-08-29 09:43:18 +0200

    added javadoc

---
+++ src/main/java/com/persistentbit/jjson/nodes/package-info.java
@@ -0,0 +1,12 @@
+/**
+ * <h1>JJNode</h1>
+ *  <p>This package contains all you need to read and write JSON in Java.</p>
+ *  <ul>
+ *      <li>The java based json representation: JJNode</li>
+ *      <li>A json parser: JJParser</li>
+ *      <li>A json printer: JJPrinter</li>
+ *  </ul>
+ *
+ *
+ */
+package com.persistentbit.jjson.nodes;
