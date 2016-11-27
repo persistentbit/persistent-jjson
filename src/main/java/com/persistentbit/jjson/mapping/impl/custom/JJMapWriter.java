4195f97fc9f2dadf51480d2f55e6a5613dad21d2
Peter Muys <peter@petermuys.com>
2016-11-27 09:42:42 +0100

    :new: Changed reading/writing of map entries to [key,value] instead of an JJNodeObject to align it with Javascript maps

--- src/main/java/com/persistentbit/jjson/mapping/impl/custom/JJMapWriter.java
+++ src/main/java/com/persistentbit/jjson/mapping/impl/custom/JJMapWriter.java
@@ -1,32 +1,29 @@
 package com.persistentbit.jjson.mapping.impl.custom;
 
-import com.persistentbit.core.collections.PMap;
 import com.persistentbit.core.collections.PStream;
 import com.persistentbit.jjson.mapping.JJWriter;
 import com.persistentbit.jjson.mapping.impl.JJObjectWriter;
 import com.persistentbit.jjson.nodes.JJNode;
 import com.persistentbit.jjson.nodes.JJNodeArray;
-import com.persistentbit.jjson.nodes.JJNodeObject;
 
 import java.util.Map;
 
 /**
+ * General Java Map Writer.<br>
+ * Writes out an Array of Array Entries [key,value]
  * @author Peter Muys
  * @since 1/09/2016
  */
-public class JJMapWriter  implements JJObjectWriter {
-    @Override
-    public JJNode write(Object value, JJWriter masterWriter) {
-        Map v = (Map) value;
+public class JJMapWriter implements JJObjectWriter{
 
-        PStream<Map.Entry> pstream = PStream.from(v.entrySet());
-        return new JJNodeArray(
-                pstream.map( e ->
-                        new JJNodeObject(
-                                PMap.<String,JJNode>empty().put("key",masterWriter.write(e.getKey())).put("value",masterWriter.write(e.getValue()))
-                        )
-                )
-        );
+	@Override
+	public JJNode write(Object value, JJWriter masterWriter) {
+		Map v = (Map) value;
 
-    }
+		PStream<Map.Entry<?, ?>> pstream = PStream.from(v.entrySet());
+		return new JJNodeArray(
+									  pstream.map(e -> new JJNodeArray(masterWriter.write(e.getKey()), masterWriter.write(e.getValue())))
+		);
+
+	}
 }
--- src/main/java/com/persistentbit/jjson/mapping/impl/custom/JJPMapReader.java
+++ src/main/java/com/persistentbit/jjson/mapping/impl/custom/JJPMapReader.java
@@ -14,57 +14,58 @@ import com.persistentbit.jjson.mapping.impl.JJObjectReader;
 import com.persistentbit.jjson.mapping.impl.JJsonException;
 import com.persistentbit.jjson.nodes.JJNode;
 import com.persistentbit.jjson.nodes.JJNodeArray;
-import com.persistentbit.jjson.nodes.JJNodeObject;
 
 import java.lang.reflect.ParameterizedType;
 import java.lang.reflect.Type;
 import java.util.function.Supplier;
 
 /**
- * User: petermuys
- * Date: 26/08/16
- * Time: 09:00
+ * General PMap Reader, reading an array of entry arrays [key,value]
+ *
+ * @author Peter Muys
+ * @since 26/08/16
  */
-public class JJPMapReader   implements JJObjectReader, JJDescriber {
+public class JJPMapReader implements JJObjectReader, JJDescriber{
 
-    private final Supplier<IPMap> ipMapSupplier;
+	private final Supplier<IPMap> ipMapSupplier;
 
-    public JJPMapReader(Supplier<IPMap> ipMapSupplier) {
-        this.ipMapSupplier = ipMapSupplier;
-    }
+	public JJPMapReader(Supplier<IPMap> ipMapSupplier) {
+		this.ipMapSupplier = ipMapSupplier;
+	}
 
-    @Override
-    public Object read(Type type, JJNode node, JJReader reader) {
-        if(node.getType() == JJNode.JType.jsonNull){
-            return null;
-        }
-        if(type instanceof ParameterizedType == false){
-            throw new JJsonException("Expected a parameterized PMap, not just a PMap");
-        }
-        ParameterizedType pt  = (ParameterizedType)type;
-        Type[] typeArgs = pt.getActualTypeArguments();
-        Type keyType = typeArgs[0];
-        Type valueType = typeArgs[1];
-        Class clsKey = ReflectionUtils.classFromType(keyType);
-        Class clsValue = ReflectionUtils.classFromType(valueType);
-        JJNodeArray arr = node.asArray().get();
-        return  ipMapSupplier.get().plusAll(arr.pstream().map(n -> {
-            JJNodeObject obj = n.asObject().get();
-            Object key = reader.read(obj.get("key").get(),clsKey,keyType);
-            Object value = reader.read(obj.get("value").get(),clsValue,valueType);
-            return new Tuple2(key,value);
-        }));
-    }
+	@SuppressWarnings("unchecked")
+	@Override
+	public Object read(Type type, JJNode node, JJReader reader) {
+		if (node.getType() == JJNode.JType.jsonNull) {
+			return null;
+		}
+		if (type instanceof ParameterizedType == false) {
+			throw new JJsonException("Expected a parameterized PMap, not just a PMap");
+		}
+		ParameterizedType pt        = (ParameterizedType) type;
+		Type[]            typeArgs  = pt.getActualTypeArguments();
+		Type              keyType   = typeArgs[0];
+		Type              valueType = typeArgs[1];
+		Class             clsKey    = ReflectionUtils.classFromType(keyType);
+		Class             clsValue  = ReflectionUtils.classFromType(valueType);
+		JJNodeArray       arr       = node.asArray().get();
+		return ipMapSupplier.get().plusAll(arr.pstream().map(n -> {
+			JJNodeArray entryArr = n.asArray().orElseThrow(() -> new RuntimeException("Expected an array"));
+			Object      key      = reader.read(entryArr.pstream().get(0), clsKey, keyType);
+			Object      value    = reader.read(entryArr.pstream().get(1), clsValue, valueType);
+			return new Tuple2(key, value);
+		}));
+	}
 
-    @Override
-    public JJTypeDescription describe(Type t, JJDescriber masterDescriber) {
-        Class cls = ReflectionUtils.classFromType(t);
+	@Override
+	public JJTypeDescription describe(Type t, JJDescriber masterDescriber) {
+		Class cls = ReflectionUtils.classFromType(t);
 
 
-        PMap<String,JJTypeSignature> td = JJDescriber.getGenericsParams(t,masterDescriber);
-        JJTypeSignature sig = new JJTypeSignature(new JJClass(cls), JJTypeSignature.JsonType.jsonMap, td);
-        PList<String> doc = PList.empty();
+		PMap<String, JJTypeSignature> td  = JJDescriber.getGenericsParams(t, masterDescriber);
+		JJTypeSignature               sig = new JJTypeSignature(new JJClass(cls), JJTypeSignature.JsonType.jsonMap, td);
+		PList<String>                 doc = PList.empty();
 
-        return new JJTypeDescription(sig,doc);
-    }
+		return new JJTypeDescription(sig, doc);
+	}
 }
